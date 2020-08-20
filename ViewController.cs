ab9dc22dca6ff17a308206b10814d36ff8d442d8
songmin350861319 <songmin350861319@gmail.com>
2020-08-20 08:57:56 +0800

    task1.1 Three Views

---
+++ ViewController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour
+{
+    
+        //对象
+        private GameObject mainCamera;
+        private GameObject camera1;
+        private GameObject camera2;
+        private GameObject camera3;
+
+        // Use this for initialization
+        void Start()
+        {
+            mainCamera = GameObject.Find("Main Camera");
+            camera1 = GameObject.Find("Camera1");
+            camera2 = GameObject.Find("Camera2");
+            camera3 = GameObject.Find("Camera3");
+
+            camera1.active = false;
+            camera2.active = false;
+            camera3.active = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnGUI()
+        {
+
+            if (GUILayout.Button("主 摄像机", GUILayout.Height(50)))
+            {
+                mainCamera.active = true;
+                camera1.active = false;
+                camera2.active = false;
+                camera3.active = false;
+            }
+            if (GUILayout.Button("左 摄像机", GUILayout.Height(50)))
+            {
+                mainCamera.active = false;
+                camera1.active = true;
+                camera2.active = false;
+                camera3.active = false;
+            }
+            if (GUILayout.Button("右 摄像机", GUILayout.Height(50)))
+            {
+                mainCamera.active = false;
+                camera1.active = false;
+                camera2.active = true;
+                camera3.active = false;
+            }
+            if (GUILayout.Button("顶部 摄像机", GUILayout.Height(50)))
+            {
+                mainCamera.active = false;
+                camera1.active = false;
+                camera2.active = false;
+                camera3.active = true;
+            }
+        }
+    
+}
